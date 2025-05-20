ff27d1c6db18d7c4eed012e795b59eebdb48b49e
unknown <vhugodossantos33@gmail.com>
2025-05-19 21:32:15 -0300

    update

---
+++ PainelAdmin/Program.cs
@@ -0,0 +1,42 @@
+using PainelAdmin.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PainelAdmin.Data;
+using Microsoft.AspNetCore.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<PainelAdminContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PainelAdminContext") ?? throw new InvalidOperationException("Connection string 'PainelAdminContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+ContextMongodb.ConnectionString = builder.Configuration.GetSection("MongoConnection:ConnectionString").Value;
+ContextMongodb.Database = builder.Configuration.GetSection("MongoConnection:Database").Value;
+ContextMongodb.IsSSL = Convert.ToBoolean(builder.Configuration.GetSection("MongoConnection:IsSSL").Value);
+
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddMongoDbStores<ApplicationUser, ApplicationRole, string>(
+    ContextMongodb.ConnectionString, ContextMongodb.Database);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
