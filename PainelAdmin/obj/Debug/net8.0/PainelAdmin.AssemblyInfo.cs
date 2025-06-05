f2c86acab04ee837cfc6df00c9be8ddf1392a371
Jhonatan <jhonatanjau98@gmail.com>
2025-06-04 22:34:52 -0300

    fix issues

--- PainelAdmin/obj/Debug/net8.0/PainelAdmin.AssemblyInfo.cs
+++ PainelAdmin/obj/Debug/net8.0/PainelAdmin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PainelAdmin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c611ff88005d085f09aa1e085c73d01f279dbbbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe23f7eccf66622e775e107a671b7f4abd9055e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PainelAdmin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PainelAdmin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
