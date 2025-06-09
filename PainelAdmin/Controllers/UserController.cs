2cfeeb6a6beabb3538548fcc5d0e54d28ba1fe6b
JHONATAN MARCELINO DE JESUS <0201392411005>
2025-06-09 10:06:32 -0300

    Create function to register users

--- PainelAdmin/Controllers/UserController.cs
+++ PainelAdmin/Controllers/UserController.cs
@@ -92,6 +92,77 @@ namespace PainelAdmin.Controllers
 
             return View(model);
         }
+        [AllowAnonymous]
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Cadastro(UsuarioAdminCadastroViewModel model)
+        {
+            if (!await _roleManager.RoleExistsAsync("USER"))
+            {
+                await _roleManager.CreateAsync(new ApplicationRole { Name = "USER" });
+            }
+
+            if (ModelState.IsValid)
+            {
+                string? nomeArquivoFoto = null;
+
+                if (model.FotoUpload != null && model.FotoUpload.Length > 0)
+                {
+                    var folder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "img", "perfil");
+                    Directory.CreateDirectory(folder);
+
+                    nomeArquivoFoto = Guid.NewGuid() + Path.GetExtension(model.FotoUpload.FileName);
+                    var caminho = Path.Combine(folder, nomeArquivoFoto);
+
+                    using var stream = new FileStream(caminho, FileMode.Create);
+                    await model.FotoUpload.CopyToAsync(stream);
+                }
+
+                var endereco = new Endereco
+                {
+                    Rua = model.Rua,
+                    Numero = model.Numero,
+                    Complemento = model.Complemento,
+                    Bairro = model.Bairro,
+                    Cidade = model.Cidade,
+                    Estado = model.Estado,
+                    CEP = model.CEP
+                };
+
+                var appUser = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    PhoneNumber = model.Telefone,
+                    DataNascimento = model.DataNascimento,
+                    Ativo = true,
+                    Nome = model.Nome,
+                    Sexo = model.Sexo,
+                    Endereco = endereco,
+                    CPF = model.CPF,
+                    Foto = nomeArquivoFoto != null ? $"/img/perfil/{nomeArquivoFoto}" : null
+                };
+
+                var result = await _userManager.CreateAsync(appUser, model.Senha);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(appUser, "ADM");
+                    TempData["MensagemSucesso"] = "Cadastro realizado com sucesso!";
+                    return RedirectToAction("Login", "Account");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
 
         [Authorize(Roles = "ADM")]
         public IActionResult CreateRole()
